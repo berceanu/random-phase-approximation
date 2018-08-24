60ba2009dc4c0faa6acbbf385ebd7e0e00625fcd
berceanu <andreicberceanu@gmail.com>
2018-08-24 15:14:37 +0300

    now running FORTRAN codes

--- draft.py
+++ draft.py
@@ -2,6 +2,8 @@
 
 import subprocess
 import sys
+import os
+import shutil
 from threading import Thread
 
 
@@ -33,47 +35,65 @@ def stream_process_stdout(process, log_fn):
 def log_fn(process):
     return lambda line: sys.stdout.write('{}: {:.2f}%\n'.format(process, float(line.split()[-1]))) if 'pa1 = ' in line else None
 
+# define working folders
+base_path = '/home/berceanu/Development/rpa'
+subfolders = (os.path.join(parent, child) for parent in ('zero_temperature', 'finite_temperature')\
+                        for child in ('ground_state', 'excited_states'))
+ztgs_dir, ztes_dir, ftgs_dir, ftes_dir = (os.path.join(base_path, subf) for subf in subfolders)
+
 if __name__ == "__main__":
 
-    # ps_one = subprocess.Popen("./run", stdout=subprocess.PIPE, bufsize=1,
-    #  cwd="./zero_temperature/excited_states/")
-    # ps_two = subprocess.Popen("./run", stdout=subprocess.PIPE, bufsize=1,
-    #  cwd="./finite_temperature/excited_states/")
+    # define blocks for FORTRAN input files
+    common_gstate_block =      ("l6       =   10                     ! output file\n"
+                                "n0f      =   20   20                ! number of oscillator shells\n"
+                                "b0       =   -1.6280835             ! oscillator parameter (fm**-1) of basis\n"
+                                "maxi     =  800                     ! maximal number of iterations\n"
+                                "xmix     =  0.2                     ! annealing parameter\n"
+                                "NI62                                ! nucleus under consideration\n"
+                               )
+    zero_temp_gstate_block =   ("Fixedgap = 00.000    00.000         ! Frozen Gapparmeter for neutr. and proton\n"
+                                "GA       = 00.000    00.000         ! Pairing-Constants GG = GA/A\n"
+                                "Init.Gap = 01.000    01.000         ! Initial values for the Gap parameters\n"
+                                "ivpair   =   1                      ! pair. ME: 0 read, 1 calc. 2 only calc.\n"
+                                "vfac     =  1.15                    ! vpair multiplication\n"
+                                "blocking:   0  2  1  1              ! Blocking neutrons: y/n, j, ip, nr\n"
+                                "blocking:   0  0  0  0              ! Blocking protons:  y/n, j, ip, nr\n"
+                               )
+    finite_temp_gstate_block = ("Delta    =  0.000   0.000           ! Gapparameter for neutrons and  protons\n"
+                                "temp     =  2.0                     ! temperature\n"
+                                "filename =  T0\n"
+                               )
+
+    # create FORTRAN input files
+    with open(os.path.join(ztgs_dir, 'dis.dat'), 'w') as f:
+        f.write(common_gstate_block)
+        f.write(zero_temp_gstate_block)
+
+    with open(os.path.join(ftgs_dir, 'dis.dat'), 'w') as f:
+        f.write(common_gstate_block)
+        f.write(finite_temp_gstate_block)
+
+    # run FORTRAN codes for zero and finite temperature ground states
+#    FNULL = open(os.devnull, 'w')
+#    procs =[subprocess.Popen("./run", stdout=FNULL, stderr=subprocess.STDOUT, cwd=path) for path in (ztgs_dir, ftgs_dir)]
+
+#    for proc in procs:
+        #proc.wait()
+
+    # copy outputs as input for C++ codes
+    src = os.path.join(ztgs_dir, 'qrpa.wel')
+    dst = ztes_dir
+    shutil.copyfile(src, dst)
+
+    src = os.path.join(ftgs_dir, 'rpa.wel')
+    dst = ftes_dir
+    shutil.copy(src, dst)
+
+    # ps_one = subprocess.Popen("./run", stdout=subprocess.PIPE, bufsize=1, cwd=ztes_dir)
+    # ps_two = subprocess.Popen("./run", stdout=subprocess.PIPE, bufsize=1, cwd=ftes_dir)
 
     # logger_one = async_stream_process_stdout(ps_one, log_fn('zero T'))
     # logger_two = async_stream_process_stdout(ps_two, log_fn('finite T'))
 
     # logger_one.join()
     # logger_two.join()
-
-
-    common_gstate_block = ("l6       =   10                     ! output file\n"
-                           "n0f      =   20   20                ! number of oscillator shells\n"
-                           "b0       =   -1.6280835             ! oscillator parameter (fm**-1) of basis\n"
-                           "maxi     =  800                     ! maximal number of iterations\n"
-                           "xmix     =  0.2                     ! annealing parameter\n"
-                           "NI62                                ! nucleus under consideration\n"
-                          )
-    
-    zero_temp_gstate_block = ("Fixedgap = 00.000    00.000         ! Frozen Gapparmeter for neutr. and proton\n"
-                              "GA       = 00.000    00.000         ! Pairing-Constants GG = GA/A\n"
-                              "Init.Gap = 01.000    01.000         ! Initial values for the Gap parameters\n"
-                              "ivpair   =   1                      ! pair. ME: 0 read, 1 calc. 2 only calc.\n"
-                              "vfac     =  1.15                    ! vpair multiplication\n"
-                              "blocking:   0  2  1  1              ! Blocking neutrons: y/n, j, ip, nr\n"
-                              "blocking:   0  0  0  0              ! Blocking protons:  y/n, j, ip, nr\n"
-                             )
-    
-    finite_temp_gstate_block = ("Delta    =  0.000   0.000           ! Gapparameter for neutrons and  protons\n"
-                                "temp     =  2.0                     ! temperature\n"
-                                "filename =  T0\n"
-                               )
-    
-
-    with open('dis.dat', 'w') as f:
-        f.write(common_gstate_block)
-        f.write(zero_temp_gstate_block)
-
-    with open('dis.dat', 'w') as f:
-        f.write(common_gstate_block)
-        f.write(finite_temp_gstate_block)
