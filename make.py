13aa18b63711277043a17ac8976db6b5d404a3a1
Andrei Berceanu <andreicberceanu@gmail.com>
2018-12-31 17:57:10 +0200

    pruned ftes

--- make.py
+++ make.py
@@ -32,29 +32,43 @@ def check_extensions(fnames, extensions):
             raise TypeError('Wrong file extension on {}'.format(f))
     return None
 
+def extract_pruned(prunefile):
+    with open(prunefile) as file:
+        logger.info('Reading %s', prunefile)
+        file_contents = file.read()
+    filenames = re.findall(fnamerex, file_contents)
+    check_extensions(filenames, ['.cc', '.h'])
+    # remove duplicates
+    filenames = list(set(filenames))
+    logger.info('%s distinct file names from pruned.make.', len(filenames))
+    return filenames
+
 def extract_from(makefile):
     with open(makefile) as file:
         logger.info('Reading %s', makefile)
         file_contents = file.read()
-        filenames = re.findall(fnamerex, file_contents)
-        logger.info('Distinct extensions: %s', distinct_extensions(filenames))
+    filenames = re.findall(fnamerex, file_contents)
+    logger.info('Distinct extensions: %s', distinct_extensions(filenames))
+    #
+    if re.search(cpprex, file_contents): # C++
+        logger.info('C++ makefile detected.')
+        check_extensions(filenames, ['.o', '.cc', '.h'])
+        logger.info('Removing all .o files.')
+        filenames = [f for f in filenames if os.path.splitext(f)[1] != '.o']
+        prunefile = os.path.join(os.path.dirname(makefile), 'pruned.make')
+        pruned_files = extract_pruned(prunefile)
         #
-        if re.search(cpprex, file_contents): # C++
-            logger.info('C++ makefile detected.')
-            check_extensions(filenames, ['.o', '.cc', '.h'])
-            logger.info('Removing all .o files.')
-            filenames = [f for f in filenames if os.path.splitext(f)[1] != '.o']
-            #
-        elif re.search(fortrex, file_contents): # Fortran
-            logger.info('Fortran makefile detected.')
-            logger.info('Renaming all .o files to .f')
-            filenames = [change_ext(f, '.o', '.f') for f in filenames]
-            #
-        else: # Otherwise
-            raise TypeError('Cannot detect type of makefile.') 
+    elif re.search(fortrex, file_contents): # Fortran
+        logger.info('Fortran makefile detected.')
+        logger.info('Renaming all .o files to .f')
+        filenames = [change_ext(f, '.o', '.f') for f in filenames]
+        #
+    else: # Otherwise
+        raise TypeError('Cannot detect type of makefile.') 
     # remove duplicates
     filenames = list(set(filenames))
     logger.info('%s distinct file names from Makefile.', len(filenames))
+    logger.info('Make files missing from pruned files: %s', list(set(filenames).difference(set(pruned_files))))
     return filenames
 
 def main(): 
@@ -73,8 +87,7 @@ def main():
     src = os.path.join(base_path, 'src')
     src_files = os.listdir(src)
     check_extensions(src_files, ['.cc', '.h'])
-    logger.info('Distinct extensions in `src`: %s', distinct_extensions(src_files))
-    logger.info('%s %s distinct file names in `src`.', len(src_files), len(set(src_files)))
+    logger.info('%s distinct file names in `src`.', len(src_files))
 
     # files in `src` but not in Makefile
     not_in_makefile = list(set(src_files).difference(set(makefilenames)))
