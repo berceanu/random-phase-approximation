b9009351876b009d1b2660a60e71237a9600887d
berceanu <andreicberceanu@gmail.com>
2019-01-28 16:26:51 +0200

    added script for invoking all release builds

---
+++ release_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd finite_temperature/ground_state/
+./release.sh
+cd ../../finite_temperature/excited_states/
+./release.sh
+cd ../../zero_temperature/ground_state/
+./release.sh
+cd ../../zero_temperature/excited_states/
+./release.sh
