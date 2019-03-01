fd37052600bb238ce4f911d054ce659421335039
berceanu <andreicberceanu@gmail.com>
2019-03-01 11:46:18 +0200

    minor cleanup

--- signac/aggregation/src/dashboard.py
+++ signac/aggregation/src/dashboard.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python3
-# Copyright (c) 2018 The Regents of the University of Michigan
-# All rights reserved.
-# This software is licensed under the BSD 3-Clause License.
 from signac_dashboard import Dashboard
 from signac_dashboard.modules.statepoint_list import StatepointList
 from signac_dashboard.modules.image_viewer import ImageViewer
 from signac_dashboard.modules.document_list import DocumentList
 from signac_dashboard.modules.file_list import FileList
 from signac_dashboard.modules.notes import Notes
-import mypackage.dipole_transitions as dt
-
 
 
 class MyDashboard(Dashboard):
@@ -26,8 +20,7 @@ if __name__ == '__main__':
         StatepointList(enabled=True),
         DocumentList(max_chars=140),
         FileList(enabled=False),
-        Notes(enabled=False),
-        # dt.DipoleTransitions(name='Dipole Transitions', enabled=False),
+        Notes(enabled=False)
         ],
         config=config
         )
