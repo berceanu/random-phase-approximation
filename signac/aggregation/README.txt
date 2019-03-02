8d03e0ef502cd55df0df530d9867aa1ab1f08e95
berceanu <andreicberceanu@gmail.com>
2019-03-02 22:45:13 +0200

    added more command line plotting options

---
+++ signac/aggregation/README.txt
@@ -0,0 +1,2 @@
+python3 src/aggregation.py --help
+python3 src/dashboard.py run --host 0.0.0.0 --port 9999
--- signac/aggregation/src/aggregation.py
+++ signac/aggregation/src/aggregation.py
@@ -4,6 +4,9 @@
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.gridspec import GridSpec
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+from matplotlib import rcParams
 from cycler import cycler
 from collections import defaultdict
 import pandas as pd
@@ -17,11 +20,15 @@ logger = logging.getLogger(__name__)
 import argparse
 import itertools as it
 
+# matplotlib settings
+min_exp = tuple(rcParams['axes.formatter.limits'])[0]
+y_formatter = ScalarFormatter(useMathText=True)
+y_formatter.set_powerlimits((min_exp, 5))
 
 line_colors = ['C0', 'C1', 'C2', 'C3']
 line_styles = ['-', '--', ':', '-.']
 
-cyl = cycler(c=line_colors) + cycler(ls=line_styles)
+cyl = cycler(color=line_colors) + cycler(linestyle=line_styles)
 vert_cyl = cycler(colors=line_colors) + cycler(linestyles=line_styles)
 
 loop_cy_iter = cyl()
@@ -33,8 +40,14 @@ vert_STYLE = defaultdict(lambda : next(vert_loop_cy_iter))
 
 
 def isofigure(job_group, isoscalar_panel=False, vlines=False,
-                minEnergy=0., maxEnergy=30., # MeV
+                minEnergy=None, maxEnergy=None, 
+                maxIsoscalarR=100000., maxIsovectorR=13., # e^2*fm^2/MeV
                 code_mapping=code_api.NameMapping()):
+
+    if not minEnergy:
+        minEnergy = 0. # MeV
+    if not maxEnergy:
+        maxEnergy = 30. # MeV
 
     plot_type = ['lorentzian']
     if vlines:
@@ -44,25 +57,30 @@ def isofigure(job_group, isoscalar_panel=False, vlines=False,
     if isoscalar_panel:
         panels.append('isoscalar')
         fig = Figure(figsize=(12, 6)) # width, height in inches
-        canvas = FigureCanvas(fig)
+        _ = FigureCanvas(fig)
         gs = GridSpec(2, 1)
         ax = {'isoscalar': fig.add_subplot(gs[0,0]),
               'isovector': fig.add_subplot(gs[1,0])}
     else: # no isoscalar panel
         fig = Figure(figsize=(10, 4))
-        canvas = FigureCanvas(fig)
+        _ = FigureCanvas(fig)
         gs = GridSpec(1, 1)
         ax = {'isovector': fig.add_subplot(gs[0,0])}
 
 
-    def isoplotax(ax):
-        ax.plot(df.energy, df.transition_strength,
-                    label=f"T = {job.sp.temperature} MeV",
-                    **STYLE[str(job.sp.temperature)])
-        if vlines:
+    def isoplotax(ax=plt.gca(), lorexc='lorentzian'):
+        if lorexc == 'excitation':
             ax.vlines(df.energy, 0., df.transition_strength,
                         label='_nolegend_',
                         **vert_STYLE[str(job.sp.temperature)])
+
+        elif lorexc == 'lorentzian':
+            ax.plot(df.energy, df.transition_strength,
+                        label=f"T = {job.sp.temperature} MeV",
+                        **STYLE[str(job.sp.temperature)])
+
+        else:
+            raise ValueError('`lorexc` must be either \'lorentzian\' or \'excitation\'')
 
 
     for job in sorted(job_group, key=lambda job: job.sp.temperature):
@@ -77,17 +95,20 @@ def isofigure(job_group, isoscalar_panel=False, vlines=False,
             for lorexc in plot_type:
                 fn = job.fn(code_mapping.out_file(temp, skalvec, lorexc))
                 df = pd.read_csv(fn, delim_whitespace=True, comment='#', skip_blank_lines=True,
-                            header=None, names=['energy', 'transition_strength'])
+                                     header=None, names=['energy', 'transition_strength'])
                 df = df[(df.energy >= minEnergy) & (df.energy <= maxEnergy)] # MeV
 
-                isoplotax(ax=ax[skalvec])
+                isoplotax(ax=ax[skalvec], lorexc=lorexc)
 
+    
     ax['isovector'].legend()
-    ax['isovector'].set(xlabel="E (MeV)")
+    ax['isovector'].set(xlabel="E (MeV)", ylim=[None, maxIsovectorR])
     if isoscalar_panel:
+        ax['isoscalar'].set(ylim=[None, maxIsoscalarR])
+        ax['isoscalar'].yaxis.set_major_formatter(y_formatter)
         fig.subplots_adjust(hspace=0.3)
    
-    return canvas
+    return fig
 
 
 
@@ -115,8 +136,9 @@ def main_groupby(args):
             statepoints.append(sp)
         const_sp = dict(set.intersection(*(set(d.items()) for d in statepoints)))
 
-        figcanvas = isofigure(gr2, isoscalar_panel=args.isoscalar, vlines=args.vlines,
+        fig = isofigure(gr2, isoscalar_panel=args.isoscalar, vlines=args.vlines,
                                    minEnergy=args.minEnergy, maxEnergy=args.maxEnergy,
+                                   maxIsoscalarR=args.maxIsoscalarR, maxIsovectorR=args.maxIsovectorR,
                                    code_mapping=code)
 
 
@@ -124,14 +146,13 @@ def main_groupby(args):
             agg_job.doc['nucleus'] = nucleus
             agg_job.doc.update(origin)
 
-            fig = figcanvas.figure
             fig.suptitle(
                 (fr"Transition strength distribution of ${{}}^{{{mass_number}}} {element} \; "
                  fr"{agg_job.sp.angular_momentum}^{{{agg_job.sp.parity}}}$")
                 )
 
             # save figure to disk in agg_job's folder
-            figcanvas.print_png(agg_job.fn('iso_all_temp_all.png'))
+            fig.canvas.print_png(agg_job.fn('iso_all_temp_all.png'))
             logger.info("wrote %s" % agg_job.fn('iso_all_temp_all.png'))
 
 
@@ -151,13 +172,19 @@ def main():
     parser.add_argument(
         '--minEnergy',
         type=float,
-        default=0.,
         help="Lower energy threshold for spectrum, in MeV.")
     parser.add_argument(
         '--maxEnergy',
         type=float,
-        default=30.,
         help="Upper energy threshold for spectrum, in MeV.")
+    parser.add_argument(
+        '--maxIsovectorR',
+        type=float,
+        help="Upper transition strength threshold for isovector spectrum, in e^2*fm^2/MeV.")
+    parser.add_argument(
+        '--maxIsoscalarR',
+        type=float,
+        help="Upper transition strength threshold for isoscalar spectrum, in e^2*fm^2/MeV.")
 
     args = parser.parse_args() # load command line args
     main_groupby(args)
