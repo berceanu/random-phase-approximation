29afa818ea61263973d4104045c09487a28f4c95
berceanu <andreicberceanu@gmail.com>
2019-03-02 20:36:39 +0200

    refactor complete

--- signac/aggregation/src/aggregation.py
+++ signac/aggregation/src/aggregation.py
@@ -31,85 +31,63 @@ STYLE = defaultdict(lambda : next(loop_cy_iter))
 vert_STYLE = defaultdict(lambda : next(vert_loop_cy_iter))
 
 
-def out_file_plot(job, temp, skalvec, lorexc, ax=None,
-                    code_mapping=code_api.NameMapping(),
-                    minEnergy=0., maxEnergy=30., # MeV
-                    ):
-    fn = job.fn(code_mapping.out_file(temp, skalvec, lorexc))
 
-    df = pd.read_csv(fn, delim_whitespace=True, comment='#', skip_blank_lines=True,
-                header=None, names=['energy', 'transition_strength'])
+def isofigure(job_group, isoscalar_panel=False, vlines=False,
+                minEnergy=0., maxEnergy=30., # MeV
+                code_mapping=code_api.NameMapping()):
 
-    df = df[(df.energy >= minEnergy) & (df.energy <= maxEnergy)] # MeV
+    plot_type = ['lorentzian']
+    if vlines:
+        plot_type.append('excitation')
 
-    if ax:
-        if lorexc == 'excitation':
-            ax.vlines(df.energy, 0., df.transition_strength,
-                        label='_nolegend_',
-                        **vert_STYLE[str(job.sp.temperature)])
-        elif lorexc == 'lorentzian':
-            ax.plot(df.energy, df.transition_strength, 
-                        label=f"T = {job.sp.temperature} MeV",
-                        **STYLE[str(job.sp.temperature)])
-        else:
-            raise ValueError
-        
-    return df
-
-
-def main_aggregation(args):
-    pass
-
-def store_aggregated_results(Z, N, rpa_jobs,
-                                     plot_type=['excitation', 'lorentzian'],
-                                     args=None,
-                                     ): # command-line arguments
-
-    nucleus = util.get_nucleus(proton_number=Z, neutron_number=N)    
-
-    is_finite = lambda job: 'finite' if job.sp.temperature > 0 else 'zero'
-    code = code_api.NameMapping()
-
-    if args.isoscalar:
+    panels = ['isovector']
+    if isoscalar_panel:
+        panels.append('isoscalar')
         fig = Figure(figsize=(12, 6)) # width, height in inches
         canvas = FigureCanvas(fig)
         gs = GridSpec(2, 1)
         ax = {'isoscalar': fig.add_subplot(gs[0,0]),
               'isovector': fig.add_subplot(gs[1,0])}
-        panels = ['isoscalar', 'isovector']
     else: # no isoscalar panel
-        fig = Figure(figsize=(12, 5)) 
+        fig = Figure(figsize=(10, 4))
         canvas = FigureCanvas(fig)
         gs = GridSpec(1, 1)
         ax = {'isovector': fig.add_subplot(gs[0,0])}
-        panels = ['isovector']
 
 
-    origin = {}
-    for job in sorted(rpa_jobs, key=lambda job: job.sp.temperature):
-        logger.info("plotting %s with T = %s MeV" % (str(job), job.sp.temperature))
-        origin[f"T = {job.sp.temperature} MeV".replace('.', '_')] = str(job)
+    def isoplotax(ax):
+        ax.plot(df.energy, df.transition_strength,
+                    label=f"T = {job.sp.temperature} MeV",
+                    **STYLE[str(job.sp.temperature)])
+        if vlines:
+            ax.vlines(df.energy, 0., df.transition_strength,
+                        label='_nolegend_',
+                        **vert_STYLE[str(job.sp.temperature)])
+
+
+    for job in sorted(job_group, key=lambda job: job.sp.temperature):
+        temp='finite' if job.sp.temperature > 0 else 'zero'
+
         for skalvec in panels:
             for sp in "top", "bottom", "right":
                 ax[skalvec].spines[sp].set_visible(False)
             ax[skalvec].set(ylabel=r"$R \; (e^2fm^2/MeV)$")
             ax[skalvec].set_title(skalvec)
+
             for lorexc in plot_type:
-                _ = out_file_plot(job=job, ax=ax[skalvec], temp=is_finite(job), 
-                                        skalvec=skalvec, lorexc=lorexc, code_mapping=code,
-                                        minEnergy=args.minEnergy, maxEnergy=args.maxEnergy,
-                                    )
+                fn = job.fn(code_mapping.out_file(temp, skalvec, lorexc))
+                df = pd.read_csv(fn, delim_whitespace=True, comment='#', skip_blank_lines=True,
+                            header=None, names=['energy', 'transition_strength'])
+                df = df[(df.energy >= minEnergy) & (df.energy <= maxEnergy)] # MeV
+
+                isoplotax(ax=ax[skalvec])
 
     ax['isovector'].legend()
     ax['isovector'].set(xlabel="E (MeV)")
-    if args.isoscalar:
+    if isoscalar_panel:
         fig.subplots_adjust(hspace=0.3)
-
-    # here job is the last one in rpa_jobs
-    element, mass = util.split_element_mass(job)
-    fig.suptitle(fr"Transition strength distribution of ${{}}^{{{mass}}} {element} \; {job.sp.angular_momentum}^{{{job.sp.parity}}}$")
-
-
+   
+    return canvas
 
 
 
@@ -119,33 +97,42 @@ def main_groupby(args):
     logger.info("rpa project: %s" % rpa.root_directory())
     logger.info("aggregation project: %s" % aggregation.root_directory())
 
-    ZN = ('proton_number', 'neutron_number')
-    for key, group in rpa.groupby(ZN):
+    code = code_api.NameMapping()
+
+    for key, group in rpa.groupby(('proton_number', 'neutron_number')):
         logger.info("(Z, N) =  (%s, %s)" % key)
+        nucleus = util.get_nucleus(*key)
+        mass_number = sum(key)
+        element = util.atomic_symbol_for_Z(key[0]).title()
 
         gr1, gr2 = it.tee(group)
 
         statepoints = []
+        origin = {}
         for job in gr1:
+            origin[f"T = {job.sp.temperature} MeV".replace('.', '_')] = str(job)
             sp = copy.deepcopy(job.sp())
-            # for k in keys:
-            #     sp.pop(k, None)
             statepoints.append(sp)
         const_sp = dict(set.intersection(*(set(d.items()) for d in statepoints)))
-        print(f"{ZN} = {key}, sp = {const_sp}")
-        # do something with gr2
 
-        # store_aggregated_results(*key, group, 
-                                    # plot_type=plot_type, args=args)
+        figcanvas = isofigure(gr2, isoscalar_panel=args.isoscalar, vlines=args.vlines,
+                                   minEnergy=args.minEnergy, maxEnergy=args.maxEnergy,
+                                   code_mapping=code)
+
 
         with aggregation.open_job(const_sp) as agg_job: # .init() implicitly called here
-            # agg_job.doc['nucleus'] = nucleus
-            # agg_job.doc.update(origin)
+            agg_job.doc['nucleus'] = nucleus
+            agg_job.doc.update(origin)
+
+            fig = figcanvas.figure
+            fig.suptitle(
+                (fr"Transition strength distribution of ${{}}^{{{mass_number}}} {element} \; "
+                 fr"{agg_job.sp.angular_momentum}^{{{agg_job.sp.parity}}}$")
+                )
 
             # save figure to disk in agg_job's folder
-            # canvas.print_png(agg_job.fn('iso_all_temp_all.png'))
+            figcanvas.print_png(agg_job.fn('iso_all_temp_all.png'))
             logger.info("wrote %s" % agg_job.fn('iso_all_temp_all.png'))
-
 
 
 
@@ -173,12 +160,8 @@ def main():
         help="Upper energy threshold for spectrum, in MeV.")
 
     args = parser.parse_args() # load command line args
-
-    plot_type = ['lorentzian']
-    if args.vlines:
-        plot_type.append('excitation')
-
     main_groupby(args)
+
 
 if __name__ == '__main__':
     logging.basicConfig(
