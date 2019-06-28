9cfd6d11a92a97307cd589c55692a3f1516f103e
berceanu <andreicberceanu@gmail.com>
2019-06-28 21:58:26 +0300

    Remove dependencies

--- signac/mypackage/setup.py
+++ signac/mypackage/setup.py
@@ -6,14 +6,7 @@ setup(
    author='Andrei Berceanu',
    author_email='andrei.berceanu@eli-np.ro',
    packages=['mypackage'],
-#    scripts=['bin/script1','bin/script2'],
-#    url='http://pypi.python.org/pypi/PackageName/',
    license='LICENSE.txt',
    description='An awesome package that does something',
    long_description=open('README.txt').read(),
-   install_requires=[
-       "signac >= 0.9.4",
-       "signac-flow >= 0.6.4",
-       "signac-dashboard >= 0.1.6",
-   ],
 )
