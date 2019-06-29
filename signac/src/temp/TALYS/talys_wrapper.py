e1a011908195524786e64632af8b1dee44155c9b
Andrei Berceanu <andreicberceanu@gmail.com>
2019-06-29 16:41:57 +0300

    Add TALYS wrapper

---
+++ signac/src/temp/TALYS/talys_wrapper.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Simple wrapper for calling TALYS code."""
+
+import numpy as np
+
+if __name__ == '__main__':
+    v = np.asarray([1, 2, 3])
+    print(v)
