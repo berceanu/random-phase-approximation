f42fe99e0988816892876d5116ecd6bf728c6a8d
Andrei Berceanu <andreicberceanu@gmail.com>
2019-07-03 22:30:30 +0300

    Revert init to complex version

--- signac/src/init.py
+++ signac/src/init.py
@@ -5,38 +5,76 @@ Iterates over all defined state points and initializes
 the associated job workspace directories."""
 import logging
 
+import signac ##
+import numpy as np
+import mypackage.code_api as code_api
 import mypackage.util as util
-import signac
-
 
 def main():
     project = signac.init_project('rpa', workspace="/media/berceanu/berc/rpa/workspace")
 
-    statepoint = dict(
-        # atomic number Z
-        proton_number=50,
-        # neutron number N
-        neutron_number=96,
-        # nucleus angular momentum
-        angular_momentum=1,  #
-        # nucleus parity
-        parity="-",  #
-        # system temperature in MeV
-        temperature=2.0,
-        # transition energy in MeV
-        transition_energy=0.42,  # 0.42 is random
-    )
-    project.open_job(statepoint).init()
+    for N in range(76, 96 + 2, 2):
+        for T in (0.0, 0.5, 1.0, 2.0):
+            statepoint = dict(
+                # atomic number Z
+                proton_number=50, # fixed atomic number
+
+                # neutron number N
+                neutron_number=N,
+
+                # nucleus angular momentum
+                angular_momentum=1, #
+
+                # nucleus parity
+                parity="-", #
+
+                # system temperature in MeV
+                temperature=T,
+
+                # transition energy in MeV
+                transition_energy=0.42 # 0.42 is random
+                )
+            project.open_job(statepoint).init()
+
+
+    for Z in range(44, 48 + 2, 2):
+        for T in (0.0, 0.5, 1.0, 2.0):
+            statepoint = dict(
+                # atomic number Z
+                proton_number=Z, 
+
+                # neutron number N
+                neutron_number=82, # fixed
+
+                # nucleus angular momentum
+                angular_momentum=1, #
+
+                # nucleus parity
+                parity="-", #
+
+                # system temperature in MeV
+                temperature=T,
+
+                # transition energy in MeV
+                transition_energy=0.42 # 0.42 is random
+                )
+            project.open_job(statepoint).init()
+
 
     for job in project:
         nprot = job.sp.proton_number
         nneutr = job.sp.neutron_number
         nucleus = util.get_nucleus(proton_number=nprot, neutron_number=nneutr)
-        job.doc.setdefault("nucleus", nucleus)
+        job.doc.setdefault('nucleus', nucleus)
 
 
-if __name__ == "__main__":
+
+    # project.write_statepoints()
+    # for job in project:
+    #     job.doc.setdefault('run_zero_temp_ground_state', True)
+    #     job.doc.setdefault('run_finite_temp_ground_state', True)
+
+
+if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     main()
-
-#
