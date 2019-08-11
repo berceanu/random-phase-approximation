912b44f2f8e8d2f59bb507a8d29c9b24a64fb9af
Andrei Berceanu <andreicberceanu@gmail.com>
2019-08-11 20:11:53 +0300

    Add minimal rpa init for testing

---
+++ signac/projects/rpa/src/small_init.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Initialize the project's data space.
+
+Iterates over all defined state points and initializes
+the associated job workspace directories."""
+import logging
+
+import signac
+import mypackage.util as util
+
+
+def main():
+    project = signac.init_project("rpa", workspace="workspace")
+
+    for Z in range(48, 50 + 2, 2):
+        for T in (0.0, 0.5, 1.0, 2.0):
+            statepoint = dict(
+                # atomic number Z
+                proton_number=Z,
+                # neutron number N
+                neutron_number=82,  # fixed
+                # nucleus angular momentum
+                angular_momentum=1,  #
+                # nucleus parity
+                parity="-",  #
+                # system temperature in MeV
+                temperature=T,
+                # transition energy in MeV
+                transition_energy=0.42,  # 0.42 is random
+            )
+            project.open_job(statepoint).init()
+
+    for job in project:
+        nprot = job.sp.proton_number
+        nneutr = job.sp.neutron_number
+        nucleus = util.get_nucleus(proton_number=nprot, neutron_number=nneutr)
+        job.doc.setdefault("nucleus", nucleus)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
