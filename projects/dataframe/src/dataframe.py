06ec1808ff2a0628ebd8a2c7c64b30ed6f3c4f05
Andrei Berceanu <andreicberceanu@gmail.com>
2019-08-19 00:18:22 +0300

    Implement main loop

--- projects/dataframe/src/dataframe.py
+++ projects/dataframe/src/dataframe.py
@@ -9,40 +9,39 @@ logger = logging.getLogger(__name__)
 
 code = code_api.NameMapping()
 
-rpa = sg.get_project(root="../rpa")
-dataframe = sg.get_project(root="./")
+model = {'zero': 'QRPA', 'finite': 'FTRPA'}
 
-logger.info("rpa project: %s" % rpa.root_directory())
-logger.info("agg project: %s" % dataframe.root_directory())
+def main():
+    rpa = sg.get_project(root="../rpa")
+    logger.info("rpa project: %s" % rpa.root_directory())
+
+    dataframes = []
+    for job in rpa.find_jobs({'proton_number': 50}):
+        temp = "finite" if job.sp.temperature > 0 else "zero"
+        print(model[temp], job.sp.neutron_number, job.sp.temperature)
+        fname = job.fn(code.out_file(temp, "isovector", "lorentzian"))
+
+        df_lorvec = pd.read_csv(
+            fname,
+            delim_whitespace=True,
+            comment="#",
+            skip_blank_lines=True,
+            header=None,
+            names=["U", "fE1"],
+        )
+        dataframes.append(df_lorvec)
+
+    print(len(dataframes))
 
 
-# fn = job.fn(code_mapping.out_file(temp, skalvec, lorexc))
-# df = pd.read_csv(
-#     fn,
-#     delim_whitespace=True,
-#     comment="#",
-#     skip_blank_lines=True,
-#     header=None,
-#     names=["energy", "transition_strength"],
-# )
-# df = df[(df.energy >= minEnergy) & (df.energy <= maxEnergy)]  # MeV
-#
-#
-#
+
 # for (z, n), group in rpa.groupby(("proton_number", "neutron_number")):
 #     logger.info("(Z, N) =  (%s, %s)" % (z, n))
 #
 #     atomic_symbol, mass_number = util.get_nucleus(z, n, joined=False)
 #
-#     gr1, gr2 = it.tee(group)
-#
-#     statepoints = []
-#     origin = {}
-#     for job in gr1:
-#         origin[f"T = {job.sp.temperature} MeV".replace(".", "_")] = str(job)
+#     for job in group:
 #         sp = job.sp._as_dict()
-#         statepoints.append(sp)
-#     const_sp = dict(set.intersection(*(set(d.items()) for d in statepoints)))
 
 
 if __name__ == "__main__":
