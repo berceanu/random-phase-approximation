d5ab94745865c2c76688de9d091016fdaf3af9e6
Andrei Berceanu <andreicberceanu@gmail.com>
2019-08-24 20:10:03 +0300

    Add dataframe __init__

---
+++ projects/dataframe/src/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Project for creating a `pandas` dataframe that will contain all the project's output data.
+This dataframe can then be operated on by user-defined functions, data in it can be aggregated
+and plotted interactively in `jupyter` notebooks.
+"""
+
+from .dataframe import main  # NOQA: F401
