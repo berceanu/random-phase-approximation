09a9b573bb8948c3c3f7e9173eb0e86bf9b4bf0f
Andrei Berceanu <andreicberceanu@gmail.com>
2020-01-28 19:09:58 +0200

    Rough draft of all plots

--- projects/dataframe/all_plots.py
+++ projects/dataframe/all_plots.py
@@ -13,6 +13,57 @@ temperatures = (0.0, 1.0, 2.0)
 ntemp = len(temperatures)
 
 
+def plot_series(ax, table, column, temperature, neutron_number, counter, label, annotation,
+                plot_type="linear-linear", energy_interval=(0.1, 10)):
+    series = table.loc[:, (column, temperature, neutron_number)]
+
+    ax.plot(
+        series.index.values,
+        series.values,
+        color=colourWheel[counter % len(colourWheel)],
+        linestyle="-",
+        dashes=dashesStyles[counter % len(dashesStyles)],
+        label=label,
+    )
+
+    ax.set_yscale(plot_type.split("-")[0])
+    ax.set_xscale(plot_type.split("-")[1])
+    ax.set_xlim(left=energy_interval[0], right=energy_interval[1])
+
+    ax.set_ylabel("$R$ %s" % units[column], labelpad=-2)
+    ax.set_xlabel("$E$ %s" % units["excitation_energy"], labelpad=-0.5)
+    ax.legend(loc="upper left", ncol=1, handlelength=1)
+    ax.annotate(s=annotation, xy=(0.7, 0.8), xycoords="axes fraction")
+
+
+def plot_table(column, table, include_talys=False, plot_type="linear-linear"):
+    for iso in isotopes:
+        fig, ax = pyplot.subplots()
+        fig.subplots_adjust(left=0.09, bottom=0.14, right=0.97, top=0.97)
+        for j, T in enumerate(temperatures):
+            plot_series(ax=ax, table=table, column=column, temperature=T, neutron_number=iso,
+                        counter=j, label="%s MeV" % T, annotation="N = %s" % iso, plot_type=plot_type)
+            if T == 0 and include_talys:
+                plot_series(ax=ax, table=table, column="tabulated_" + column, temperature=T, neutron_number=iso,
+                            counter=niso + j, label="%s MeV TALYS" % T, annotation="N = %s" % iso, plot_type=plot_type)
+
+        fig.set_size_inches(width, height)
+        fig.savefig("N_%s_all_T_%s.pdf" % (iso, column))
+
+    for T in temperatures:
+        fig, ax = pyplot.subplots()
+        fig.subplots_adjust(left=0.09, bottom=0.14, right=0.97, top=0.97)
+        for j, iso in enumerate(isotopes):
+            plot_series(ax=ax, table=table, column=column, temperature=T, neutron_number=iso,
+                        counter=j, label="%s" % iso, annotation="T = %s MeV" % T, plot_type=plot_type)
+            if T == 0 and include_talys:
+                plot_series(ax=ax, table=table, column="tabulated_" + column, temperature=T, neutron_number=iso,
+                            counter=niso + j, label="%s TALYS" % iso, annotation="T = %s MeV" % T, plot_type=plot_type)
+
+        fig.set_size_inches(width, height)
+        fig.savefig("T_%s_all_N_%s.pdf" % (T, column))
+
+
 def main():
     ee_data = (
         pd.read_hdf(df_path, "excitation_energy")
@@ -24,19 +75,10 @@ def main():
             / u_factor,
         )
     )
-    ne_data = (
-        pd.read_hdf(df_path, "neutron_energy")
-        .query("neutron_number in @isotopes and temperature in @temperatures")
-        .assign(mass_number=lambda frame: frame.proton_number + frame.neutron_number)
-    )
-
     df = ee_data.drop(
         columns=[
             "mass_number",
             "proton_number",
-            # "tabulated_strength_function_mb",
-            # "tabulated_strength_function_fm",
-            # "strength_function_mb",
         ]
     )
     table = pd.pivot_table(
@@ -52,105 +94,97 @@ def main():
     )
 
     ###
-    for iso in isotopes:
-        fig, ax = pyplot.subplots()
-        fig.subplots_adjust(left=0.09, bottom=0.14, right=0.97, top=0.97)
 
-        for j, T in enumerate(temperatures):
-            series = table.loc[:, ("strength_function_fm", T, iso)]
+    # plot_table(column="strength_function_fm", table=table, include_talys=True, plot_type="log-log")
+    # plot_table(column="strength_function_mb", table=table, include_talys=True)
 
-            ax.plot(
-                series.index.values,
-                series.values,
-                color=colourWheel[j % len(colourWheel)],
-                linestyle="-",
-                dashes=dashesStyles[j % len(dashesStyles)],
-                label="%s MeV" % T,
-            )
+    # cross_section
+    ne_data = (
+        pd.read_hdf(df_path, "neutron_energy")
+        .query("neutron_number in @isotopes and temperature in @temperatures")
+        .assign(mass_number=lambda frame: frame.proton_number + frame.neutron_number)
+        .rename(columns={"cross_section_talys": "tabulated_cross_section"})
+    )
+    df = ne_data.drop(
+        columns=[
+            "mass_number",
+            "proton_number",
+            "capture_rate",
+            "capture_rate_talys",
+        ]
+    )
+    table = pd.pivot_table(
+        df,
+        index=["neutron_energy"],
+        values=[
+            "cross_section",
+            "tabulated_cross_section",
+        ],
+        columns=["temperature", "neutron_number"],
+    )
 
-        ax.set_ylabel("$R$ %s" % units["strength_function_fm"], labelpad=-2)
-        ax.set_xlabel("$E$ %s" % units["excitation_energy"], labelpad=-0.5)
-        ax.legend(loc="upper left", ncol=1, handlelength=1)
-        ax.annotate(s="N = %s" % iso, xy=(0.7, 0.8), xycoords="axes fraction")
+    # plot_table(column="cross_section", table=table, include_talys=True, plot_type="log-log")
 
-        fig.set_size_inches(width, height)
-        fig.savefig("N_%s_all_T.pdf" % iso)
+    # capture rate
+    df = ne_data.drop(
+        columns=[
+            "proton_number",
+            "cross_section",
+            "tabulated_cross_section",
+            "neutron_energy",
+        ]
+    )
+    table = pd.pivot_table(
+        df,
+        index=["neutron_number"],
+        values=[
+            "capture_rate",
+            "capture_rate_talys",
+        ],
+        columns=["temperature"]
+    )
+    print(table)
+    print(table.index)
+    print(table.columns)
+    print(table.loc[:, ("capture_rate", 0.0)])
 
-    for T in temperatures:
-        fig, ax = pyplot.subplots()
-        fig.subplots_adjust(left=0.09, bottom=0.14, right=0.97, top=0.97)
+    fig, ax = pyplot.subplots()
+    fig.subplots_adjust(left=0.09, bottom=0.14, right=0.97, top=0.97)
+    for j, T in enumerate(temperatures):
+        series = table.loc[:, ("capture_rate", T)]
 
-        for j, iso in enumerate(isotopes):
-            series = table.loc[:, ("strength_function_fm", T, iso)]
+        ax.plot(
+            series.index.values,
+            series.values,
+            color=colourWheel[j % len(colourWheel)],
+            linestyle="-",
+            dashes=dashesStyles[j % len(dashesStyles)],
+            label=str(T),
+        )
 
-            ax.plot(
-                series.index.values,
-                series.values,
-                color=colourWheel[j % len(colourWheel)],
-                linestyle="-",
-                dashes=dashesStyles[j % len(dashesStyles)],
-                label=str(iso),
-            )
+    table = pd.pivot_table(
+        df,
+        index=["temperature"],
+        values=[
+            "capture_rate",
+            "capture_rate_talys",
+        ],
+        columns=["neutron_number"]
+    )
 
-        ax.set_ylabel("$R$ %s" % units["strength_function_fm"], labelpad=-2)
-        ax.set_xlabel("$E$ %s" % units["excitation_energy"], labelpad=-0.5)
-        ax.legend(loc="upper left", ncol=1, handlelength=1)
-        ax.annotate(s="T = %s MeV" % T, xy=(0.7, 0.8), xycoords="axes fraction")
+    fig, ax = pyplot.subplots()
+    fig.subplots_adjust(left=0.09, bottom=0.14, right=0.97, top=0.97)
+    for j, iso in enumerate(isotopes):
+        series = table.loc[:, ("capture_rate", iso)]
 
-        fig.set_size_inches(width, height)
-        fig.savefig("T_%s_all_N.pdf" % T)
-
-    ###
-    for iso in isotopes:
-        fig, ax = pyplot.subplots()
-        fig.subplots_adjust(left=0.09, bottom=0.14, right=0.97, top=0.97)
-
-        for j, T in enumerate(temperatures):
-            series = table.loc[:, ("strength_function_mb", T, iso)]
-
-            ax.plot(
-                series.index.values,
-                series.values,
-                color=colourWheel[j % len(colourWheel)],
-                linestyle="-",
-                dashes=dashesStyles[j % len(dashesStyles)],
-                label="%s MeV" % T,
-            )
-
-        ax.set_ylabel("$R$ %s" % units["strength_function_mb"], labelpad=-2)
-        ax.set_xlabel("$E$ %s" % units["excitation_energy"], labelpad=-0.5)
-        ax.legend(loc="upper left", ncol=1, handlelength=1)
-        ax.annotate(s="N = %s" % iso, xy=(0.7, 0.8), xycoords="axes fraction")
-
-        fig.set_size_inches(width, height)
-        fig.savefig("N_%s_all_T_mb.pdf" % iso)
-
-    for T in temperatures:
-        fig, ax = pyplot.subplots()
-        fig.subplots_adjust(left=0.09, bottom=0.14, right=0.97, top=0.97)
-
-        for j, iso in enumerate(isotopes):
-            series = table.loc[:, ("strength_function_mb", T, iso)]
-
-            ax.plot(
-                series.index.values,
-                series.values,
-                color=colourWheel[j % len(colourWheel)],
-                linestyle="-",
-                dashes=dashesStyles[j % len(dashesStyles)],
-                label=str(iso),
-            )
-
-        ax.set_ylabel("$R$ %s" % units["strength_function_mb"], labelpad=-2)
-        ax.set_xlabel("$E$ %s" % units["excitation_energy"], labelpad=-0.5)
-        ax.legend(loc="upper left", ncol=1, handlelength=1)
-        ax.annotate(s="T = %s MeV" % T, xy=(0.7, 0.8), xycoords="axes fraction")
-
-        fig.set_size_inches(width, height)
-        fig.savefig("T_%s_all_N_mb.pdf" % T)
-
-    # repeat everything above, for cross_section
-    print(ne_data)
+        ax.plot(
+            series.index.values,
+            series.values,
+            color=colourWheel[j % len(colourWheel)],
+            linestyle="-",
+            dashes=dashesStyles[j % len(dashesStyles)],
+            label=str(iso),
+        )
 
 
 if __name__ == "__main__":
