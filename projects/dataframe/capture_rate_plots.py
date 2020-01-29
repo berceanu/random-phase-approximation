7a98b3e7994196ea68eceb19a9f1f846034f90af
Andrei Berceanu <andreicberceanu@gmail.com>
2020-01-29 21:02:18 +0200

    Finish all capture rate plots

--- projects/dataframe/capture_rate_plots.py
+++ projects/dataframe/capture_rate_plots.py
@@ -50,54 +50,80 @@ def main():
     table = pd.pivot_table(
         df,
         index=["neutron_number"],
-        values=[
-            "capture_rate",
-            "capture_rate_talys",  # todo also plot this one
-        ],
-        columns=["temperature"]
+        values=["capture_rate", "capture_rate_talys"],
+        columns=["temperature"],
     )
 
     fig, ax = pyplot.subplots()
-    fig.subplots_adjust(left=0.12, bottom=0.14, right=0.97, top=0.97)
+    fig.subplots_adjust(left=0.10, bottom=0.14, right=0.97, top=0.97)
     for j, T in enumerate(temperatures):
-        plot_capture_rate_vs_n(ax=ax, column="capture_rate", table=table, temperature=T, counter=j)
+        plot_capture_rate_vs_n(
+            ax=ax, column="capture_rate", table=table, temperature=T, counter=j
+        )
     fig.set_size_inches(width, height)
     fig.savefig("capture_rate_vs_N.pdf")
 
     fig, ax = pyplot.subplots()
-    fig.subplots_adjust(left=0.12, bottom=0.14, right=0.97, top=0.97)
+    fig.subplots_adjust(left=0.10, bottom=0.14, right=0.97, top=0.97)
     for j, T in enumerate(temperatures[1:]):
-        plot_capture_rate_vs_n(ax=ax, column="capture_rate_talys", table=table, temperature=T, counter=j)
+        plot_capture_rate_vs_n(
+            ax=ax, column="capture_rate_talys", table=table, temperature=T, counter=j
+        )
     fig.set_size_inches(width, height)
     fig.savefig("capture_rate_vs_N_TALYS.pdf")
 
-    # table = pd.pivot_table(
-    #     df,
-    #     index=["temperature"],
-    #     values=[
-    #         "capture_rate",
-    #         "capture_rate_talys",
-    #     ],
-    #     columns=["neutron_number"]
-    # )
-    #
-    # print(table)
-    # print(table.index)
-    # print(table.columns)
-    #
-    # fig, ax = pyplot.subplots()
-    # fig.subplots_adjust(left=0.09, bottom=0.14, right=0.97, top=0.97)
-    # for j, iso in enumerate(isotopes):
-    #     series = table.loc[:, ("capture_rate", iso)]
-    #
-    #     ax.plot(
-    #         series.index.values,
-    #         series.values,
-    #         color=colourWheel[j % len(colourWheel)],
-    #         linestyle="-",
-    #         dashes=dashesStyles[j % len(dashesStyles)],
-    #         label=str(iso),
-    #     )
+    table = pd.pivot_table(
+        df,
+        index=["temperature"],
+        values=["capture_rate", "capture_rate_talys"],
+        columns=["neutron_number"],
+    )
+
+    def plot_capture_rate_vs_t(ax, column, table, isotope, counter, xticks):
+        series = table.loc[:, (column, isotope)]
+
+        ax.plot(
+            series.index.values,
+            series.values,
+            "-o",
+            color=colourWheel[counter % len(colourWheel)],
+            dashes=dashesStyles[counter % len(dashesStyles)],
+            label="%s" % isotope,
+        )
+        ax.set_yscale("log")
+        ax.xaxis.set_major_locator(ticker.FixedLocator(xticks))
+        ax.set_ylabel("Neutron Capture Rate %s" % units[column], labelpad=-2)
+        ax.set_xlabel("$T$ %s" % units["temperature"], labelpad=-0.5)
+
+        ax.legend(loc="upper right", ncol=1, handlelength=1)
+
+    fig, ax = pyplot.subplots()
+    fig.subplots_adjust(left=0.10, bottom=0.14, right=0.97, top=0.97)
+    for j, iso in enumerate((76, 86, 96)):
+        plot_capture_rate_vs_t(
+            ax=ax,
+            column="capture_rate",
+            table=table,
+            isotope=iso,
+            counter=j,
+            xticks=(0.0, 0.5, 1.0, 1.5, 2.0),
+        )
+    fig.set_size_inches(width, height)
+    fig.savefig("capture_rate_vs_T.pdf")
+
+    fig, ax = pyplot.subplots()
+    fig.subplots_adjust(left=0.10, bottom=0.14, right=0.97, top=0.97)
+    for j, iso in enumerate((76, 86, 96)):
+        plot_capture_rate_vs_t(
+            ax=ax,
+            column="capture_rate_talys",
+            table=table,
+            isotope=iso,
+            counter=j,
+            xticks=(0.5, 1.0, 1.5, 2.0),
+        )
+    fig.set_size_inches(width, height)
+    fig.savefig("capture_rate_vs_T_TALYS.pdf")
 
 
 if __name__ == "__main__":
