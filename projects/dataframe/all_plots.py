92604bbb3f7af78e1e9b673a57eee3a9773c3f56
Andrei Berceanu <andreicberceanu@gmail.com>
2020-01-30 13:46:13 +0200

    Reformatted via black

--- projects/dataframe/all_plots.py
+++ projects/dataframe/all_plots.py
@@ -13,8 +13,19 @@ temperatures = (0.0, 1.0, 2.0)
 ntemp = len(temperatures)
 
 
-def plot_series(ax, table, column, temperature, neutron_number, counter, label, annotation,
-                plot_type="linear-linear", energy_interval=(0.1, 10), ylabel=None):  # "$R$ %s" % units[column]
+def plot_series(
+    ax,
+    table,
+    column,
+    temperature,
+    neutron_number,
+    counter,
+    label,
+    annotation,
+    plot_type="linear-linear",
+    energy_interval=(0.1, 10),
+    ylabel=None,
+):  # "$R$ %s" % units[column]
     series = table.loc[:, (column, temperature, neutron_number)]
 
     ax.plot(
@@ -37,16 +48,46 @@ def plot_series(ax, table, column, temperature, neutron_number, counter, label, 
     ax.annotate(s=annotation, xy=(0.7, 0.8), xycoords="axes fraction")
 
 
-def plot_table(column, table, include_talys=False, talys_column=None, plot_type="linear-linear", energy_interval=(0.1, 10), ylabel=None):   # "$R$ %s" % units[column]
+def plot_table(
+    column,
+    table,
+    include_talys=False,
+    talys_column=None,
+    plot_type="linear-linear",
+    energy_interval=(0.1, 10),
+    ylabel=None,
+):  # "$R$ %s" % units[column]
     for iso in isotopes:
         fig, ax = pyplot.subplots()
         fig.subplots_adjust(left=0.12, bottom=0.14, right=0.97, top=0.97)
         for j, T in enumerate(temperatures):
-            plot_series(ax=ax, table=table, column=column, temperature=T, neutron_number=iso,
-                        counter=j, label="%s MeV" % T, annotation="N = %s" % iso, plot_type=plot_type, energy_interval=energy_interval, ylabel=ylabel)
+            plot_series(
+                ax=ax,
+                table=table,
+                column=column,
+                temperature=T,
+                neutron_number=iso,
+                counter=j,
+                label="%s MeV" % T,
+                annotation="N = %s" % iso,
+                plot_type=plot_type,
+                energy_interval=energy_interval,
+                ylabel=ylabel,
+            )
             if T == 0 and include_talys:
-                plot_series(ax=ax, table=table, column=talys_column, temperature=T, neutron_number=iso,
-                            counter=niso + j, label="TALYS %s MeV" % T, annotation="N = %s" % iso, plot_type=plot_type, energy_interval=energy_interval, ylabel=ylabel)
+                plot_series(
+                    ax=ax,
+                    table=table,
+                    column=talys_column,
+                    temperature=T,
+                    neutron_number=iso,
+                    counter=niso + j,
+                    label="TALYS %s MeV" % T,
+                    annotation="N = %s" % iso,
+                    plot_type=plot_type,
+                    energy_interval=energy_interval,
+                    ylabel=ylabel,
+                )
         fig.set_size_inches(width, height)
         fig.savefig("N_%s_all_T_%s.pdf" % (iso, column))
 
@@ -54,11 +95,33 @@ def plot_table(column, table, include_talys=False, talys_column=None, plot_type=
         fig, ax = pyplot.subplots()
         fig.subplots_adjust(left=0.12, bottom=0.14, right=0.97, top=0.97)
         for j, iso in enumerate(isotopes):
-            plot_series(ax=ax, table=table, column=column, temperature=T, neutron_number=iso,
-                        counter=j, label="%s" % iso, annotation="T = %s MeV" % T, plot_type=plot_type, energy_interval=energy_interval, ylabel=ylabel)
+            plot_series(
+                ax=ax,
+                table=table,
+                column=column,
+                temperature=T,
+                neutron_number=iso,
+                counter=j,
+                label="%s" % iso,
+                annotation="T = %s MeV" % T,
+                plot_type=plot_type,
+                energy_interval=energy_interval,
+                ylabel=ylabel,
+            )
             if T == 0 and include_talys:
-                plot_series(ax=ax, table=table, column=talys_column, temperature=T, neutron_number=iso,
-                            counter=niso + j, label="TALYS %s" % iso, annotation="T = %s MeV" % T, plot_type=plot_type, energy_interval=energy_interval, ylabel=ylabel)
+                plot_series(
+                    ax=ax,
+                    table=table,
+                    column=talys_column,
+                    temperature=T,
+                    neutron_number=iso,
+                    counter=niso + j,
+                    label="TALYS %s" % iso,
+                    annotation="T = %s MeV" % T,
+                    plot_type=plot_type,
+                    energy_interval=energy_interval,
+                    ylabel=ylabel,
+                )
         fig.set_size_inches(width, height)
         fig.savefig("T_%s_all_N_%s.pdf" % (T, column))
 
@@ -74,12 +137,7 @@ def main():
             / u_factor,
         )
     )
-    df = ee_data.drop(
-        columns=[
-            "mass_number",
-            "proton_number",
-        ]
-    )
+    df = ee_data.drop(columns=["mass_number", "proton_number"])
     table = pd.pivot_table(
         df,
         index=["excitation_energy"],
@@ -92,9 +150,26 @@ def main():
         columns=["temperature", "neutron_number"],
     )
 
-    plot_table(column="strength_function_fm", talys_column="tabulated_strength_function_fm", table=table, include_talys=True, plot_type="log-linear", energy_interval=(0, 20), ylabel="$R$ %s" % units["strength_function_fm"])
-    # plot_table(column="strength_function_mb", talys_column="tabulated_strength_function_mb", table=table, include_talys=True, plot_type="log-log", energy_interval=(0.1, 10), ylabel="$R$ %s" % units["strength_function_mb"])
-
+    plot_table(
+        column="strength_function_fm",
+        talys_column="tabulated_strength_function_fm",
+        table=table,
+        include_talys=True,
+        plot_type="log-linear",
+        energy_interval=(0, 20),
+        ylabel="$R$ %s" % units["strength_function_fm"],
+    )
+    #
+    #    plot_table(
+    #        column="strength_function_mb",
+    #        talys_column="tabulated_strength_function_mb",
+    #        table=table,
+    #        include_talys=True,
+    #        plot_type="log-log",
+    #        energy_interval=(0.1, 10),
+    #        ylabel="$R$ %s" % units["strength_function_mb"],
+    #    )
+    #
     # cross_section
     ne_data = (
         pd.read_hdf(df_path, "neutron_energy")
@@ -102,25 +177,24 @@ def main():
         .assign(mass_number=lambda frame: frame.proton_number + frame.neutron_number)
     )
     df = ne_data.drop(
-        columns=[
-            "mass_number",
-            "proton_number",
-            "capture_rate",
-            "capture_rate_talys",
-        ]
+        columns=["mass_number", "proton_number", "capture_rate", "capture_rate_talys"]
     )
     table = pd.pivot_table(
         df,
         index=["neutron_energy"],
-        values=[
-            "cross_section",
-            "cross_section_talys",
-        ],
+        values=["cross_section", "cross_section_talys"],
         columns=["temperature", "neutron_number"],
     )
 
-    plot_table(column="cross_section", talys_column="cross_section_talys", table=table, include_talys=True,
-               plot_type="log-log", energy_interval=(0, 20), ylabel="Cross-Section %s" % units["cross_section"])
+    plot_table(
+        column="cross_section",
+        talys_column="cross_section_talys",
+        table=table,
+        include_talys=True,
+        plot_type="log-log",
+        energy_interval=(0, 20),
+        ylabel="Cross-Section %s" % units["cross_section"],
+    )
 
 
 if __name__ == "__main__":
