1ef28b143895b2d92b1daff9ad23cb41da5a41b1
Andrei Berceanu <andreicberceanu@gmail.com>
2020-02-19 22:38:22 +0200

    Fix some TODOs

--- projects/dataframe/all_plots_niu.py
+++ projects/dataframe/all_plots_niu.py
@@ -78,19 +78,23 @@ def plot_table(
             )
             ax.legend(loc="lower left")
         else:
-            # TODO replace "N=.." with nucleus symbol ${}^{A}Sn$
-            ax.annotate(s="N = %s" % iso, xy=(0.1, 0.9), xycoords="axes fraction")
+            mass_number_final = 50 + iso
+            ax.annotate(
+                s=r"${}^{%s}$Sn" % mass_number_final,
+                xy=(0.1, 0.9),
+                xycoords="axes fraction",
+            )
             ax.legend(loc="lower right")
-        fig.set_size_inches(width, width / golden_ratio)  # TODO make subplots shorter
-        fig.savefig("N_%s_all_T_%s.pdf" % (iso, column))
+        fig.set_size_inches(width, width / golden_ratio)
+        fig.savefig("N_%s_all_T_%s" % (iso, column))
 
 
 # TODO add new set of figures savefig("T_%s_all_N_%s.pdf" % (T, column)), see all_plots.py
-# TODO label the 3 curves by nucleus symbol ${}^{A}Sn$ in legend
+# TODO label the 3 curves by nucleus symbol ${}^{A}$Sn in legend
 # TODO use E_n instead of E for cross section x axis label
-# TODO use same axes ranges as in "N_%s_all_T_%s.pdf"
+# TODO use same axes ranges as in "N_%s_all_T_%s"
 # TODO move "T=.." annotations to area with no curves
-# TODO use dual GridSpec to produce the final figure directly
+# TODO use GridSpec to produce the final figure
 
 
 def main():
--- projects/dataframe/capture_rate_plots_niu.py
+++ projects/dataframe/capture_rate_plots_niu.py
@@ -4,37 +4,35 @@ from figstyle import colourWheel, dashesStyles, width, golden_ratio
 from dataash5 import df_path, units
 from matplotlib import pyplot, ticker
 
-isotopes = list(range(76, 98, 2))
-niso = len(isotopes)
-
-# TODO remove 0.5
-temperatures = (0.5, 1.0, 2.0)
-ntemp = len(temperatures)
+temperatures = (1.0, 2.0)
+MARKERS = {"1.0": "o", "2.0": "s"}
 
 
-def plot_capture_rate_vs_n(ax, column, table, temperature, counter):
+def plot_capture_rate_vs_n(ax, column, table, temperature, counter, mass_numbers):
     series = table.loc[:, (column, temperature)]
 
     ax.plot(
         series.index.values,
         series.values,
-        "-o",
+        marker=MARKERS[str(temperature)],
         color=colourWheel[counter % len(colourWheel)],
         dashes=dashesStyles[counter % len(dashesStyles)],
         label="T = %s MeV" % temperature,
     )
     ax.set_yscale("log")
-    ax.xaxis.set_major_locator(ticker.FixedLocator(isotopes))
+    ax.xaxis.set_major_locator(ticker.FixedLocator(mass_numbers))
     ax.set_ylabel("Neutron Capture Rate %s" % units[column])
-    ax.set_xlabel("N")
-    ax.set_xlim(left=isotopes[0] - 1, right=isotopes[-1] + 1)
+    ax.set_xlabel("A")
+    ax.set_xlim(left=mass_numbers[0] - 1, right=mass_numbers[-1] + 1)
 
     ax.legend(loc="upper right", ncol=1, handlelength=1)
 
 
 def main():
-    ne_data = pd.read_hdf(df_path, "neutron_energy").query(
-        "neutron_number in @isotopes and temperature in @temperatures"
+    ne_data = (
+        pd.read_hdf(df_path, "neutron_energy")
+        .query("temperature in @temperatures")
+        .assign(mass_number=lambda frame: frame.proton_number + frame.neutron_number)
     )
     df = ne_data.drop(
         columns=[
@@ -45,17 +43,21 @@ def main():
             "neutron_energy",
         ]
     )
+    massnum = df["mass_number"].unique()
     table = pd.pivot_table(
-        df, index=["neutron_number"], values=["capture_rate"], columns=["temperature"]
+        df, index=["mass_number"], values=["capture_rate"], columns=["temperature"]
     )
 
-    # TODO use different symbols: triangle, square etc (not just circles)
-    # TODO use A instead of N for x axis label
     fig, ax = pyplot.subplots()
     fig.subplots_adjust(left=0.14, bottom=0.14, right=0.97, top=0.97)
     for j, T in enumerate(temperatures):
         plot_capture_rate_vs_n(
-            ax=ax, column="capture_rate", table=table, temperature=T, counter=j
+            ax=ax,
+            column="capture_rate",
+            table=table,
+            temperature=T,
+            counter=j + 1,
+            mass_numbers=massnum,
         )
     ax.annotate(s="Sn", xy=(0.1, 0.6), xycoords="axes fraction")
     fig.set_size_inches(width, width / golden_ratio)
