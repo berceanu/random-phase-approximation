975440b222ec9c862141ca93c380b7341c373b2c
Andrei Berceanu <andreicberceanu@gmail.com>
2020-02-19 18:11:57 +0200

    Move all matplotlib configuration to .mplstyle and matplotlibrc

--- figstyle/figstyle/__init__.py
+++ figstyle/figstyle/__init__.py
@@ -1,13 +1,9 @@
 """
 Package containing plotting customizations and routines.
 """
-import matplotlib as mpl
-
-mpl.use("TkAgg")
-
-import pkg_resources  # noqa: E402
-from matplotlib import pyplot  # noqa: E402
-from figstyle.style import colourWheel, dashesStyles, width, height  # NOQA: F401
+import pkg_resources  # NOQA: E402
+from matplotlib import pyplot  # NOQA: E402
+from figstyle.style import colourWheel, dashesStyles, width, golden_ratio  # NOQA: F401
 from figstyle.turbo_colormap import mpl_turbo_data  # NOQA: F401
 
 # Load style file
@@ -15,18 +11,6 @@ style_path = pkg_resources.resource_filename(
     "figstyle", "data/paper_double_fig.mplstyle"
 )
 pyplot.style.use(style_path)
-
-# use nice LaTeX fonts
-pyplot.rcParams.update(
-    {
-        "pgf.texsystem": "pdflatex",
-        "pgf.preamble": [
-            r"\usepackage[utf8x]{inputenc}",
-            r"\usepackage[T1]{fontenc}",
-            r"\usepackage{cmbright}",
-        ],
-    }
-)
 
 # register the "turbo" colormap
 pyplot.register_cmap(name="turbo", data=mpl_turbo_data, lut=256)
