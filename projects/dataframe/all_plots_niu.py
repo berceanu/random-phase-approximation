f127efaa1f192cd142c5801d8cd4a1a7336222c2
Andrei Berceanu <andreicberceanu@gmail.com>
2020-02-21 01:00:31 +0200

    Finish preliminary gridspec

--- projects/dataframe/all_plots_niu.py
+++ projects/dataframe/all_plots_niu.py
@@ -5,15 +5,9 @@ import pandas as pd
 
 from figstyle import colourWheel, dashesStyles, width, golden_ratio
 from dataash5 import df_path, units
-from matplotlib import pyplot
 from matplotlib.figure import Figure
 from matplotlib.gridspec import GridSpec
-
-isotopes = (76, 86, 96)
-niso = len(isotopes)
-
-temperatures = (0.0, 1.0, 2.0)
-ntemp = len(temperatures)
+from matplotlib import ticker
 
 
 @dataclass(frozen=True)
@@ -30,88 +24,9 @@ class AxesParameters:
     xscale: str
     xlim: Tuple[float, float]
     ylim: Tuple[float, float]
-    legend_loc: str
     ann: Annotation
     yscale: str = "log"
     line_label: str = "T = %s MeV"
-
-
-def plot_series(
-    ax,
-    table,
-    column,
-    temperature,
-    neutron_number,
-    counter,
-    label,
-    ylabel,
-    xlabel,
-    plot_type="linear-linear",
-    energy_interval=(0.1, 10),
-):
-    series = table.loc[:, (column, temperature, neutron_number)]
-
-    ax.plot(
-        series.index.values,
-        series.values,
-        color=colourWheel[counter % len(colourWheel)],
-        linestyle="-",
-        dashes=dashesStyles[counter % len(dashesStyles)],
-        label=label,
-    )
-
-    ax.set_yscale(plot_type.split("-")[0])
-    ax.set_xscale(plot_type.split("-")[1])
-
-    ax.set_ylabel(ylabel)
-    ax.set_xlabel(xlabel)
-    ax.set_xlim(left=energy_interval[0], right=energy_interval[1])
-    if column == "strength_function_fm":
-        ax.set_ylim(3e-2, 1.2e1)
-    elif column == "cross_section":
-        ax.set_ylim(1e-4, 1e3)
-
-
-def plot_table(
-    column, table, ylabel, xlabel, plot_type="linear-linear", energy_interval=(0.1, 10)
-):
-    for iso in isotopes:
-        fig, ax = pyplot.subplots()
-        fig.subplots_adjust(left=0.15, bottom=0.09, right=0.97, top=0.97)
-        for j, T in enumerate(temperatures):
-            plot_series(
-                ax=ax,
-                table=table,
-                column=column,
-                temperature=T,
-                neutron_number=iso,
-                counter=j,
-                label="T = %s MeV" % T,
-                plot_type=plot_type,
-                energy_interval=energy_interval,
-                ylabel=ylabel,
-                xlabel=xlabel,
-            )
-        if column == "cross_section":
-            mass_number_target = 50 + iso - 1
-            mass_number_final = mass_number_target + 1
-            ax.annotate(
-                s=r"${}^{%s}$Sn(n,$\gamma$)${}^{%s}$Sn"
-                % (mass_number_target, mass_number_final),
-                xy=(0.6, 0.9),
-                xycoords="axes fraction",
-            )
-            ax.legend(loc="lower left")
-        else:
-            mass_number_final = 50 + iso
-            ax.annotate(
-                s=r"${}^{%s}$Sn" % mass_number_final,
-                xy=(0.1, 0.9),
-                xycoords="axes fraction",
-            )
-            ax.legend(loc="lower right")
-        fig.set_size_inches(width, width / golden_ratio)
-        fig.savefig("N_%s_all_T_%s" % (iso, column))
 
 
 # TODO add new set of figures savefig("T_%s_all_N_%s.pdf" % (T, column)), see all_plots.py
@@ -123,67 +38,30 @@ def plot_table(
 
 
 def main():
-    sfunc_prm = AxesParameters(
-        ylabel="$R$ %s" % units["strength_function_fm"],
-        xlabel="E %s" % units["excitation_energy"],
-        xscale="linear",
-        xlim=(0.0, 20.0),
-        ylim=(3e-2, 1.2e1),
-        legend_loc="lower right",
-        ann=Annotation(s=r"${}^{%s}$Sn", xy=(0.1, 0.9)),
-    )
-    xsec_prm = AxesParameters(
-        ylabel="Cross-Section %s" % units["cross_section"],
-        xlabel="E$_n$ %s" % units["neutron_energy"],
-        xscale="log",
-        xlim=(1e-3, 20.0),
-        ylim=(1e-4, 1e3),
-        legend_loc="lower left",
-        ann=Annotation(s=r"${}^{%s}$Sn(n,$\gamma$)${}^{%s}$Sn", xy=(0.6, 0.9)),
-    )
-    print(sfunc_prm)
-    print(xsec_prm)
-
-    fig = Figure(constrained_layout=True)
-    gs = GridSpec(
-        nrows=3,
-        ncols=2,
-        figure=fig,
-        height_ratios=[1.0, 1.0, 1.0],
-        width_ratios=[1.0, 1.0],
-    )
-    for i, g in enumerate(gs[:, 0]):
-        ax = fig.add_subplot(g)
-        ax.text(0.5, 0.5, f"ax{i + 1:d}", va="center", ha="center")
-        print(ax)
-
-    # for i, ax in enumerate(fig.axes):  # same axes order as above
-    #     ax.text(0.5, 0.5, f"ax{i + 1:d}", va="center", ha="center")
-
-    fig.set_size_inches(width, width * golden_ratio)
-    fig.savefig("grid_spec")
+    isotopes = (76, 86, 96)
+    temperatures = (0.0, 1.0, 2.0)
 
     # strength function
     ee_data = pd.read_hdf(df_path, "excitation_energy").query(
         "neutron_number in @isotopes and temperature in @temperatures"
     )
     df = ee_data.drop(columns=["proton_number", "tabulated_strength_function_mb"])
-    table = pd.pivot_table(
+    sfunc_table = pd.pivot_table(
         df,
         index=["excitation_energy"],
         values=["strength_function_fm"],
         columns=["temperature", "neutron_number"],
     )
-    plot_table(
-        column="strength_function_fm",
-        table=table,
-        plot_type="log-linear",
-        energy_interval=(0, 20),
+    sfunc_prm = AxesParameters(
         ylabel="$R$ %s" % units["strength_function_fm"],
         xlabel="E %s" % units["excitation_energy"],
+        xscale="linear",
+        xlim=(0.0, 20.0),
+        ylim=(3e-2, 1.2e1),
+        ann=Annotation(s=r"${}^{%s}$Sn", xy=(0.5, 0.2)),
     )
 
-    # cross_section
+    # cross section
     ne_data = pd.read_hdf(df_path, "neutron_energy").query(
         "neutron_number in @isotopes and temperature in @temperatures"
     )
@@ -195,20 +73,158 @@ def main():
             "cross_section_talys",
         ]
     )
-    table = pd.pivot_table(
+    xsec_table = pd.pivot_table(
         df,
         index=["neutron_energy"],
         values=["cross_section"],
         columns=["temperature", "neutron_number"],
     )
-    plot_table(
-        column="cross_section",
-        table=table,
-        plot_type="log-log",
-        energy_interval=(1e-3, 20),
+    xsec_prm = AxesParameters(
         ylabel="Cross-Section %s" % units["cross_section"],
         xlabel="E$_n$ %s" % units["neutron_energy"],
+        xscale="log",
+        xlim=(1e-3, 20.0),
+        ylim=(1e-4, 1e3),
+        ann=Annotation(s=r"${}^{%s}$Sn(n,$\gamma$)${}^{%s}$Sn", xy=(0.1, 0.2)),
     )
+
+    # GridSpec Figure
+    nrows = len(isotopes)
+    nlines = len(temperatures)
+
+    fig = Figure(figsize=(width, width * golden_ratio))  # constrained_layout=True
+    gs = GridSpec(
+        nrows=nrows,
+        ncols=2,
+        figure=fig,
+        height_ratios=[1.0, 1.0, 1.0],
+        width_ratios=[1.0, 1.0],
+    )
+    gs.update(wspace=0.4, hspace=0.05, bottom=0.06, left=0.0, right=1.02)
+    for row in range(nrows):
+        # strength function
+        ax_left = fig.add_subplot(gs[row, 0])
+
+        for line in range(nlines):
+            series = sfunc_table.loc[
+                :, ("strength_function_fm", temperatures[line], isotopes[row])
+            ]
+            ax_left.plot(
+                series.index.values,
+                series.values,
+                color=colourWheel[line % len(colourWheel)],
+                linestyle="-",
+                dashes=dashesStyles[line % len(dashesStyles)],
+                label=sfunc_prm.line_label % temperatures[line],
+            )
+        ax_left.set_ylabel(sfunc_prm.ylabel)
+        ax_left.set_xlabel(sfunc_prm.xlabel)
+
+        ax_left.set_ylim(sfunc_prm.ylim)
+        ax_left.set_xlim(sfunc_prm.xlim)
+        ax_left.set_yscale(sfunc_prm.yscale)
+        ax_left.set_xscale(sfunc_prm.xscale)
+
+        ax_left.annotate(
+            s=sfunc_prm.ann.s % (50 + isotopes[row]),
+            xy=sfunc_prm.ann.xy,
+            xycoords=sfunc_prm.ann.xycoords,
+        )
+
+        if row != nrows - 1:  # all but the bottom panel
+            ax_left.xaxis.set_major_formatter(ticker.NullFormatter())
+            ax_left.xaxis.label.set_visible(False)
+
+        # cross section
+        ax_right = fig.add_subplot(gs[row, 1])
+
+        for line in range(nlines):
+            series = xsec_table.loc[
+                :, ("cross_section", temperatures[line], isotopes[row])
+            ]
+            ax_right.plot(
+                series.index.values,
+                series.values,
+                color=colourWheel[line % len(colourWheel)],
+                linestyle="-",
+                dashes=dashesStyles[line % len(dashesStyles)],
+                label=xsec_prm.line_label % temperatures[line],
+            )
+        ax_right.set_ylabel(xsec_prm.ylabel)
+        ax_right.set_xlabel(xsec_prm.xlabel)
+
+        ax_right.set_ylim(xsec_prm.ylim)
+        ax_right.set_xlim(xsec_prm.xlim)
+        ax_right.set_yscale(xsec_prm.yscale)
+        ax_right.set_xscale(xsec_prm.xscale)
+
+        ax_right.annotate(
+            s=xsec_prm.ann.s % ((50 + isotopes[row] - 1), (50 + isotopes[row])),
+            xy=xsec_prm.ann.xy,
+            xycoords=xsec_prm.ann.xycoords,
+        )
+
+        if row != nrows - 1:  # all but the bottom panel
+            ax_right.xaxis.set_major_formatter(ticker.NullFormatter())
+            ax_right.xaxis.label.set_visible(False)
+
+    handles, labels = fig.axes[-1].get_legend_handles_labels()
+    fig.legend(handles, labels, loc="upper center")
+
+    fig.savefig("grid_spec")
+
+    # # strength function
+    # for iso in isotopes:
+    #     for counter, T in enumerate(temperatures):
+    #         series = table.loc[:, ("strength_function_fm", T, iso)]
+    #         ax.plot(
+    #             series.index.values,
+    #             series.values,
+    #             color=colourWheel[counter % len(colourWheel)],
+    #             linestyle="-",
+    #             dashes=dashesStyles[counter % len(dashesStyles)],
+    #             label=("T = %s MeV" % T),
+    #         )
+    #         ax.set_yscale("log")
+    #         ax.set_xscale("linear")
+    #         ax.set_ylabel("$R$ %s" % units["strength_function_fm"])
+    #         ax.set_xlabel("E %s" % units["excitation_energy"])
+    #         ax.set_xlim(left=0, right=20)
+    #         ax.set_ylim(3e-2, 1.2e1)
+    #     ax.annotate(
+    #         s=r"${}^{%s}$Sn" % (50 + iso),
+    #         xy=(0.1, 0.9),
+    #         xycoords="axes fraction",
+    #     )
+    #     ax.legend(loc="lower right")
+
+    # # cross section
+    # for o in isotopes:
+    #     fig2, ax1 = pyplot.subplots()
+    #     fig2.subplots_adjust(left=0.15, bottom=0.09, right=0.97, top=0.97)
+    #     for j1, t in enumerate(temperatures):
+    #         series1 = table.loc[:, ("cross_section", t, o)]
+    #         ax1.plot(
+    #             series1.index.values,
+    #             series1.values,
+    #             color=colourWheel[j1 % len(colourWheel)],
+    #             linestyle="-",
+    #             dashes=dashesStyles[j1 % len(dashesStyles)],
+    #             label=("T = %s MeV" % t),
+    #         )
+    #         ax1.set_yscale("log")
+    #         ax1.set_xscale("log")
+    #         ax1.set_ylabel("Cross-Section %s" % units["cross_section"])
+    #         ax1.set_xlabel("E$_n$ %s" % units["neutron_energy"])
+    #         ax1.set_xlim(left=1e-3, right=20)
+    #         ax1.set_ylim(1e-4, 1e3)
+    #     ax1.annotate(
+    #         s=r"${}^{%s}$Sn(n,$\gamma$)${}^{%s}$Sn"
+    #           % ((50 + o - 1), (50 + o)),
+    #         xy=(0.6, 0.9),
+    #         xycoords="axes fraction",
+    #     )
+    #     ax1.legend(loc="lower left")
 
 
 if __name__ == "__main__":
